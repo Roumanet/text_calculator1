02ce24a6d69b25b0b8d17adb275aa8a5e802d459
Roumanet <rr8558120@gmail.com>
2025-10-02 13:52:59 +0300

    comm

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ print('niger')
 print('cyka')
 
 x = 76
+
+n = int(input())
+for i in range(n):
+    s = input()
+    if s == '' or s.isspace():
+        print(f'{i+1}: COMMENT SHOULD BE DELETED')
+    else:
+        print(f'{i+1}: {s}')
+

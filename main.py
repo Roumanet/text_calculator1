027dc90ce388b0174a9ed458ef8f64ec7d71e371
Roumanet <rr8558120@gmail.com>
2025-10-02 13:53:45 +0300

    nick

--- main.py
+++ main.py
@@ -12,3 +12,12 @@ for i in range(n):
     else:
         print(f'{i+1}: {s}')
 
+s = input()
+c = 0
+for i in s:
+    if i in 'qwertyuiopasdfghjklzxcvbnm' or i in '0123456789':
+        c += 1
+if s[0] == '@' and 5 <= len(s) <= 15 and c == len(s)-1:
+    print('Correct')
+else:
+    print('Incorrect')
